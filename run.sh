00303cd94b65f4c48e8d803484011265933fad56
Jason Koh <bk7749@gmail.com>
2018-01-24 13:30:03 -0800

    typo

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-java -cp target/VirtJenaTest-0.0.1-SNAPSHOT-fat.jar virtjena_regenerator.Tester
+java -cp target/VirtJenaTest-0.0.1-SNAPSHOT-jar-with-dependencies.jar virtjena_regenerator.Tester
